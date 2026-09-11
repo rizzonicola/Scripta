cf459a2dfcc80610dbda0ce5da35105ac7b926cc
rizzonicola <298361728+rizzonicola@users.noreply.github.com>
2026-09-12 00:55:44 +0200

    Add import for SelectedContent from rendering package

--- lib/features/editor/presentation/markdown_rendered_view.dart
+++ lib/features/editor/presentation/markdown_rendered_view.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show SelectedContent;
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
