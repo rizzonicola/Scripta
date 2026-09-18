7d3cb8e6081f440458837493576b0d9cd9a06cbd
rizzonicola <298361728+rizzonicola@users.noreply.github.com>
2026-09-18 23:58:44 +0200

    bug_fix

--- lib/features/editor/presentation/markdown_rendered_view.dart
+++ lib/features/editor/presentation/markdown_rendered_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart'
     show cupertinoTextSelectionControls, cupertinoDesktopTextSelectionControls;
